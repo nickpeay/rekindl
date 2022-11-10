5553cf6761252f9d65ab6a60ab0c5de463ba61fb
Nick Peay <nickpeay@gmail.com>
2022-11-10 15:16:20 -0500

    feat: adds progress bar to nav changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
 import Layout from '../components/layout/layout';
+import NProgress from 'nprogress/nprogress';
+
+import '../styles/globals.scss';
+import '../styles/nprogress.scss';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
